5fe3de9b65f74904b495be88dd1d93685e4e6475
Idaho Edokpayi <idaho@tridentprofessionalservices.com>
2020-02-05 18:02:55 -0500

    added email contact

--- src/components/ContactPerson.tsx
+++ src/components/ContactPerson.tsx
@@ -1,9 +1,28 @@
 import React from "react"
 import Img from "gatsby-image"
 import IContactPersonProps from "./IContactPersonProps"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
-export default function ContactPerson(props: IContactPersonProps): JSX.Element{
-    return <article className="contact">
-        
+export default function ContactPerson(props: IContactPersonProps): JSX.Element {
+  const { emailAddress, name, profileImage, title } = props
+  return (
+    <article className="contact">
+      {profileImage ? (
+        <div className="profile-image">
+          <h2>{name}</h2>
+          <Img fluid={profileImage} />
+          <span className="caption">{title}</span>
+        </div>
+      ) : (
+        <div className={"profile-image"}>
+          <h2>{name}</h2>
+          <span className="caption">{title}</span>
+        </div>
+      )}
+      <div className="social">
+        <a href={"mailto:" + emailAddress}><FontAwesomeIcon icon={faEnvelope} /></a>
+      </div>
     </article>
+  )
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ContactPerson from "../components/ContactPerson"
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact Us" />
     <h1>Contact Us</h1>
-    
+    <ContactPerson emailAddress={"mose@gideontech.com"} name={"Mose Richardson"} title={"Principal Operations Architect"} />
+    <ContactPerson emailAddress={"idaho@tridentprofessionalservices.com"} name={"Idaho Edokpayi"} title={"Chief ProductArchitect"} />
   </Layout>
 )
 
